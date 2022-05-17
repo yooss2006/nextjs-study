7930768dd366ead3ae7768e0474caf1f4d5d12d0
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-05-17 09:42:28 +0900

    css styled jsx  방식

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "934162e729cb7bcc"; }
+/******/ 		__webpack_require__.h = function() { return "b34baf59f457dad8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,33 +1,26 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css"; //css 모듈
 export default function NavBar() {
   const router = useRouter();
   return (
-    // 프로퍼티 형식으로 작성
-    //충돌을 ㅏㄴ들지 않음
-    //다른 컴포넌트에서 해당 이름이 간으하다.
-
     <nav>
       <Link href="/">
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/" ? styles.active : ""
-          }`}
-        >
-          home
-        </a>
+        <a className={router.pathname === "/" ? "active" : ""}>home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={[
-            styles.link,
-            router.pathname === "/about" ? styles.active : "",
-          ].join(" ")}
-        >
-          about
-        </a>
+        <a className={router.pathname === "/about" ? "active" : ""}>about</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: red;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
